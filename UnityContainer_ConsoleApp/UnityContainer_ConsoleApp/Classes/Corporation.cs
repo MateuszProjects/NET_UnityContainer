8a8460cc3ae49a512e061cd47ff8c288f2b9ca07
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-04-24 22:41:52 +0200

    Commit

---
+++ UnityContainer_ConsoleApp/UnityContainer_ConsoleApp/Classes/Corporation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityContainer_ConsoleApp.Classes
+{
+    public class Corporation : Interfaces.ICorporation
+    {
+        public Corporation()
+        {
+
+        }
+    }
+}
---
+++ UnityContainer_ConsoleApp/UnityContainer_Test_Class/Classes/CorporationTestsCorporation.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityContainer_ConsoleApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityContainer_ConsoleApp.Classes.Tests
+{
+    [TestClass]
+    public class CorporationTestsCorporation
+    {
+        [TestMethod]
+        public void CorporationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
